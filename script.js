3229bf25f7080c420aaf3c227bdf325e667329b5
mohammad-saberi <85442250+mohammad-saberi@users.noreply.github.com>
2021-06-07 12:18:12 +0430

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
 function compute()
 {
     p = document.getElementById("principal").value;
+    r = document.getElementById("rate").value;
+    y = document.getElementById("years").value;
+    result = document.getElementById("result");
     
+    function num() {
+        var v=document.getElementById("principal").value;
+        if(v<=0)
+        {
+            alert('value should be a positive number')
+        }
+    }
+    
+   
+function sliderChange(val) {
+document.getElementById('rate').value;
 }
-        
+
+    
+    var dateobj = new Date();
+    var B = dateobj.getFullYear();
+    year = B+y;
+
+               
+    var interest = (p*r*y)/100;
+
+
+    
+    var amt=interest;
+    var result=document.getElementById("result");
+    result.innerHTML="If you deposit "+p+",<br> at an interest rate of "+r+"%. <br>You will receive an amount of " +amt+",<br>in the year "+year;
+}
