f01f1dd3dc960f45d1bd5ec11ae28ba001ac6cc8
mohammad-saberi <85442250+mohammad-saberi@users.noreply.github.com>
2021-06-07 13:27:12 +0430

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,24 @@
-function compute()
+function updateRate() // function for slider automization
 {
-    p = document.getElementById("principal").value;
-    r = document.getElementById("rate").value;
-    y = document.getElementById("years").value;
-    result = document.getElementById("result");
-    
-    function num() {
-        var v=document.getElementById("principal").value;
-        if(v<=0)
-        {
-            alert('value should be a positive number')
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText = rateval;
+}
+
+function compute() //function for button compute interest
+{
+    var checkprin = document.getElementById("principal").value; // catch value of principal
+        if(checkprin <= 0){ // check if value is < or = 0
+            alert("Please enter a positive number");
+            document.getElementById("principal").focus();
+            return false;
         }
+    //return true; // if all ok return true
+    else {
+    var principal = document.getElementById("principal").value; // catch value of principal
+    var rate = document.getElementById("rate").value; // catch value of rate
+    var years = document.getElementById("years").value; // catch value of years
+    var amount = (principal*years*rate / 100); // compute of amount
+    var year = new Date().getFullYear()+parseInt(years); // compute years from now to selected in future
+    document.getElementById("result").innerHTML="If you deposit <mark>"+principal+"</mark>,\<br\>At an interest rate of <mark>"+rate+"%</mark>.\<br\>You will receive an amount of <mark>"+amount+"</mark>,\<br\>in the year <mark>"+year+"</mark>.\<br\>";
     }
-    
-   
-function sliderChange(val) {
-document.getElementById('rate').value;
-}
-
-    
-    var dateobj = new Date();
-    var B = dateobj.getFullYear();
-    year = B+y;
-
-               
-    var interest = (p*r*y)/100;
-
-
-    
-    var amt=interest;
-    var result=document.getElementById("result");
-    result.innerHTML="If you deposit "+p+",<br> at an interest rate of "+r+"%. <br>You will receive an amount of " +amt+",<br>in the year "+year;
-}
+}    
